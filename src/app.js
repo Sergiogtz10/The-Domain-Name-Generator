8a5cb6e24a2944c75a99eccb1024b502e2d1c293
Sergiogtz10 <sergiogutierrezcn@gmail.com>
2021-12-18 11:38:24 +0000

    excersice complete

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let first = ["the", "our", "your"];
+  let second = ["complex", "essay", "cool"];
+  let third = ["code", "algorithm", "task"];
+  let fourth = [".com", ".es", ".net"];
+
+  first.forEach(first => {
+    second.forEach(second => {
+      third.forEach(third => {
+        fourth.forEach(fourth => {
+          document.querySelector(
+            "#item"
+          ).innerHTML += `${first}${second}${third}${fourth}<br>`;
+        });
+      });
+    });
+  });
 };
